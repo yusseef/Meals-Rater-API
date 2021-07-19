c65d68d278b8d84f8ae802a65cdb2e232f389480
yusseef <yussef.raouf11@gmail.com>
2021-07-19 17:51:39 +0200

    avrg ratings and add rating in meal url

--- api/models.py
+++ api/models.py
@@ -6,19 +6,32 @@ class Meal(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(meal = self)
+        return(len(ratings))
+
+    def avg_rating(self):
+        #sum of ratings / len
+        sum = 0
+        ratings = Rating.objects.filter(meal=self) #number of ratings to the meal
+        for x in ratings:
+            sum += x.stars
+        if len(ratings) > 0:
+            return sum / len(ratings)
+
+        else:
+            return 0
+
     def __str__(self):
         return self.title
 
 class Rating(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    stars = models.IntegerField(validators=[MaxValueValidator, MinValueValidator])
+    stars = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
 
-    #def __str__(self):
-     #   return self.meal
-
-class Meta:
-    unique_together = (('user', 'meal'),)
-    index_together = (('user', 'meal'),)
+    class Meta:
+        unique_together = (('user', 'meal'),)
+        index_together = (('user', 'meal'),)
 
 
--- api/views.py
+++ api/views.py
@@ -1,12 +1,52 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Rating, Meal
 from .serializers import RatingSerializer, MealSerializer
-
-
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class  = MealSerializer
+
+    @action(methods=['POST'], detail=True)
+    def rate_meal(self, request, pk=None):
+        if 'stars' in request.data:
+            '''
+            Create or update
+            '''
+            meal = Meal.objects.get(pk=pk)
+            stars = request.data['stars']
+            username = request.data['username']
+            user = User.objects.get(username=username)
+            try:
+                #UPDATE
+                rating = Rating.objects.get(user=user.id, meal=meal.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'message': 'Stars updated successfully',
+                    'result': serializer.data
+                }
+                return Response(json, status=status.HTTP_200_OK)
+            except:
+                #CREATE
+                rating = Rating.objects.create(stars=stars, user=user, meal=meal)
+                serializer = RatingSerializer(rating, many=False)
+                json = {
+                    'message': 'Stars added successfully',
+                    'result': serializer.data
+                }
+                return Response(json, status=status.HTTP_200_OK)
+
+        else:
+            json={
+                'message':'There is no stars provided'
+            }
+            return Response(json, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
